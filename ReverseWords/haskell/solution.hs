50da041869f077de6e90f884c05b2f78b803da50
Casey McGuire <casey.jay.mcguire@gmail.com>
2015-05-19 17:37:43 -0700

    ReverseWords is solved

--- ReverseWords/haskell/solution.hs
+++ ReverseWords/haskell/solution.hs
@@ -2,8 +2,8 @@ import System.Environment (getArgs)
 
 main = do
   [inpFile] <- getArgs
-  input <- readFile inpFile
-
+  file <- readFile inpFile
+  mapM_ putStrLn $ map unwords $ map reverseWords $ filter (/=[]) $ map words $ lines file
 
 reverseWords [] = []
-reverseWords (x:xs) = (reverseWords xs):x
+reverseWords (x:xs) = (reverseWords xs) ++ [x]
