5f0b690c2c8fd21b3b0eefa3ad5709525bc1998d
Casey McGuire <casey.jay.mcguire@gmail.com>
2015-06-07 23:44:08 -0700

    Finished SumOfPrimes in clojure but it uses too much memory

---
+++ SumOfPrimes/clojure/solution.clj
@@ -0,0 +1,19 @@
+(defn is-prime? 
+ [n]
+ "Determines if the passed number is prime"
+ (defn evenly-divisible?
+   [div]
+   (if (== (mod n div) 0)
+     true
+     false))
+ (cond
+   (== 1 n) false
+   (and (== (mod n 2) 0) (not= n 2)) false ;2 is a prime number so we have to add check 
+   (not= (count (filter evenly-divisible? (range 3 (- n 1) 2))) 0) false
+   :else true))
+
+
+(println 
+ (reduce +
+  (take 1000
+        (filter is-prime? (range)))))
